1b6bf6ccd3e91bd2326498e416e0706830389b66
Francisco Sokol <chico.sokol@gmail.com>
2014-01-30 14:22:08 -0200

    done

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+
+gem "httparty", "~> 0.12.0"
+gem "nokogiri", "~> 1.6.1"
+
+
+
---
+++ install.rb
@@ -0,0 +1,30 @@
+require 'httparty'
+require 'nokogiri'
+
+class SBoInstaller
+  def install(url)
+    name = url.split('/').last
+    response = HTTParty.get(url)
+    document = Nokogiri::HTML(response.body)
+    links = document.css(".center a")
+    source = links[1].attr('href')
+    slackbuild = links[2].attr('href')
+
+    `
+    mkdir #{name}
+    cd #{name}
+    wget #{slackbuild}
+    tar xzf #{name}.tar.gz
+    cd #{name}
+    wget #{source}
+    chmod +x ./#{name}.SlackBuild
+    ./#{name}.SlackBuild
+    `
+
+  end
+
+end
+
+
+installer = SBoInstaller.new
+installer.install(ARGV[0])
